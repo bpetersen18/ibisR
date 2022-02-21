3fbc3677339d5399f0ea13c7b4ca8b68a1e65959
Bryan Petersen <bryan20@iastate.edu>
2022-02-21 16:19:58 -0600

    Created a function that creates a timeseries plot of the output from *output.R functions

---
+++ create_ibis_ts_plot.R
@@ -0,0 +1,21 @@
+# create_ibis_ts_plot
+# By: Bryan Petersen
+# Date: 02.21.22
+# Purpose: Creates a timeseries plot of the output from *output.R functions
+
+create_ibis_ts_plot <- function(data_tbl){
+  # Load libraries
+  require(tidyverse)
+  require(lubridate)
+  
+  # Convert date from a character to a date
+  data_tbl <- data_tbl %>% 
+    mutate(date = ymd(date))
+  
+  # Plot
+  p1 <- ggplot(data = data_tbl, mapping = aes(x = date, y = variable_data)) +
+    geom_line() +
+    labs(x = "Date", y = paste0(unique(data_tbl$variable_name)))
+  
+  return(p1)
+}
